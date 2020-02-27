e4f35cc1f4dde369dad33a18dbe655415dd9c119
Vo1z <Vo1z@users.noreply.github.com>
2020-02-27 21:17:33 +0100

    Map Generator 0.6
    Map generator class was added

--- Spy-do/Assets/Scripts/MapGeneration/MapGenerationController.cs
+++ Spy-do/Assets/Scripts/MapGeneration/MapGenerationController.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/*
+ * Sirex production code:
+ * Written by Voiz (Viktor Lishchuk)
+ * Email: vitya.voody@gmail.com
+ * Twitter: @V0IZ_
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MapGeneration;
@@ -19,24 +25,33 @@ class MapGenerationController : MonoBehaviour
     public GameObject leftTopBrinkCorner;
     public GameObject rightTopBrinkCorner;
 
-    public int roomHeight;
-    public int roomLength;
+    public GameObject innerObject;
+
+    public int numberOfRooms;
+    public int spaceBetweenRooms;
+    public int minRoomHeightY;
+    public int maxRoomHeightY;
+    public int minRoomLengthX;
+    public int maxRoomLengthX;
 
     private Room room;
+    private Location location;
 
     void Awake()
     {
-        room = new Room(roomHeight, roomLength);  
+        room = new Room(Random.Range(minRoomHeightY, maxRoomHeightY), Random.Range(minRoomLengthX, maxRoomLengthX));
+        location = new Location(numberOfRooms, spaceBetweenRooms, minRoomHeightY, maxRoomHeightY, minRoomLengthX, maxRoomLengthX);
     }
 
     void Start()
     {
-        createRoomLayer(room.GetRoomLayer(0).LayerObjectMap, room.GetRoomLayer(0).LayerHeightY, room.GetRoomLayer(0).LayerLengthX);
-        createRoomLayer(room.GetRoomLayer(1).LayerObjectMap, room.GetRoomLayer(1).LayerHeightY, room.GetRoomLayer(1).LayerLengthX);
-        createRoomLayer(room.GetRoomLayer(2).LayerObjectMap, room.GetRoomLayer(2).LayerHeightY, room.GetRoomLayer(2).LayerLengthX);
+        createMap(location, location.LocationLevelZ, location.LocationHeightY, location.LocationLengthX);
+
+        //createRoom(room);
+
     }
 
-    private void createRoomLayer(string[,] objectMap, int heightY, int lengthX) 
+    private void createLayer(string[,] objectMap, int heightY, int lengthX) 
     {
         for (int y = 0; y < heightY; y++) 
         {
@@ -71,8 +86,103 @@ class MapGenerationController : MonoBehaviour
                     case "topWallBrink":
                         Instantiate(topWallBrink, new Vector2(x, y), Quaternion.identity);
                         break;
+                    case "innerObject":
+                        Instantiate(innerObject, new Vector2(x, y), Quaternion.identity);
+                        break;
                 }
             }
         }
     }
+
+    private void createRoom(Room room)
+    {
+        for (int layerNumber = 0; layerNumber < room.Layers.Count; layerNumber++)
+        {
+            for (int y = 0; y < room.Layers[layerNumber].LayerHeightY; y++)
+            {
+                for (int x = 0; x < room.Layers[layerNumber].LayerLengthX; x++)
+                {
+                    switch (room.Layers[layerNumber].LayerObjectMap[y, x])
+                    {
+                        case "floor":
+                            Instantiate(floor, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "wall":
+                            Instantiate(wall, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "leftWall":
+                            Instantiate(leftWall, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "rightWall":
+                            Instantiate(rightWall, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "leftCorner":
+                            Instantiate(leftCorner, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "rightCorner":
+                            Instantiate(rightCorner, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "leftTopBrinkCorner":
+                            Instantiate(leftTopBrinkCorner, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "rightTopBrinkCorner":
+                            Instantiate(rightTopBrinkCorner, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "topWallBrink":
+                            Instantiate(topWallBrink, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "innerObject":
+                            Instantiate(innerObject, new Vector2(x, y), Quaternion.identity);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void createMap(Location location, int levelZ, int heightY, int lengthX)
+    {
+        for (int z = 0; z < levelZ; z++) 
+        {
+            for (int y = 0; y < heightY; y++)
+            {
+                for (int x = 0; x < lengthX; x++)
+                {
+                    switch (location.LocationObjectMap[z, y, x])
+                    {
+                        case "floor":
+                            Instantiate(floor, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "wall":
+                            Instantiate(wall, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "leftWall":
+                            Instantiate(leftWall, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "rightWall":
+                            Instantiate(rightWall, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "leftCorner":
+                            Instantiate(leftCorner, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "rightCorner":
+                            Instantiate(rightCorner, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "leftTopBrinkCorner":
+                            Instantiate(leftTopBrinkCorner, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "rightTopBrinkCorner":
+                            Instantiate(rightTopBrinkCorner, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "topWallBrink":
+                            Instantiate(topWallBrink, new Vector2(x, y), Quaternion.identity);
+                            break;
+                        case "innerObject":
+                            Instantiate(innerObject, new Vector2(x, y), Quaternion.identity);
+                            break;
+                    }
+                }
+            }
+        }
+    }        
 }
--- Spy-do/Assets/Scripts/MapGeneration/MapGenerationLibrary.cs
+++ Spy-do/Assets/Scripts/MapGeneration/MapGenerationLibrary.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/*
+ * Sirex production code:
+ * Written by Voiz (Viktor Lishchuk)
+ * Email: vitya.voody@gmail.com
+ * Twitter: @V0IZ_
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,11 +16,11 @@ namespace MapGeneration
         public readonly int RoomHeightY;                                         //Variable that stores width of the room of Y dimension
         public readonly int RoomLengthX;                                        //Variable that stores width of the room of X dimension
 
-        private List<Layer> layers = new List<Layer>();
+        public readonly List<Layer> Layers = new List<Layer>();
 
         public Room(int roomHeightY, int roomLengthX)      //Constructor
         {
-            this.RoomHeightY = roomHeightY - 1;
+            this.RoomHeightY = roomHeightY;
             this.RoomLengthX = roomLengthX;
             instRoom();
         }
@@ -35,50 +41,39 @@ namespace MapGeneration
             }
             else
             {
-                layers.Add(new Layer(layerHeightY, layerLenghtX)); // not sure
+                Layers.Add(new Layer(layerHeightY, layerLenghtX)); // not sure
             }
         }
 
         public void RemoveRoomLayer(int numberOfLayer)
         {
-            layers.RemoveAt(numberOfLayer); //not sure
+            Layers.RemoveAt(numberOfLayer); //not sure
         }
 
-        public Layer GetRoomLayer(int layerNumber)
+        protected void instRoom()
         {
-            return layers[layerNumber];
+            //========================Layer 0========================
+            AddRoomLayer(RoomHeightY - 1, RoomLengthX);
+            Layers[0].FillWholeLayerMap("floor");
+            Layers[0].SetVerticalLayerLine(0, "null");
+            Layers[0].SetVerticalLayerLine(RoomLengthX - 1, "null");
+
+            //========================Layer 1========================
+            AddRoomLayer(RoomHeightY - 1, RoomLengthX);
+            Layers[1].SetOnRandomLayerID("innerObject");
+
+            //========================Layer 2========================
+            AddRoomLayer(RoomHeightY, RoomLengthX);
+            Layers[2].SetHorizontalLayerLine(0, "wall");
+            Layers[2].SetHorizontalLayerLine(1, "topWallBrink");
+            Layers[2].SetHorizontalLayerLine(RoomHeightY - 2, "wall");
+            Layers[2].SetHorizontalLayerLine(RoomHeightY - 1, "topWallBrink");
+            Layers[2].SetVerticalLayerLine(0, "leftWall");
+            Layers[2].SetVerticalLayerLine(RoomLengthX - 1, "rightWall");
+            Layers[2].SetUniqueCorners("leftTopBrinkCorner", "rightTopBrinkCorner", "leftCorner", "rightCorner");
+
         }
-
-        private void instRoom() //shit is here 
-        {
-            //======================Layer 0======================
-            AddRoomLayer(RoomHeightY - 1, RoomLengthX - 1);
-            layers[0].FillWholeLayerMap("floor");
-            layers[0].SetHorizontalLayerLine(layers[0].LayerHeightY - 1, "wall");
-            layers[0].SetVerticalLayerLine(0, "leftWall");
-            layers[0].SetVerticalLayerLine(layers[0].LayerLengthX - 1, "rightWall");
-
-            //======================Layer 1======================
-            AddRoomLayer(RoomHeightY - 1, RoomLengthX - 1);
-            
-
-            //======================Layer 2======================
-            AddRoomLayer(RoomHeightY, RoomLengthX - 1);
-            //Sets top side of the room
-            layers[2].SetHorizontalLayerLine(layers[2].LayerHeightY - 1, "topWallBrink");
-            layers[2].SetOnUniqueLayerID(layers[2].LayerHeightY - 1, 0, "leftTopBrinkCorner");
-            layers[2].SetOnUniqueLayerID(layers[2].LayerHeightY - 1, layers[2].LayerLengthX - 1, "rightTopBrinkCorner");
-            //Sets bottom sides
-            layers[2].SetHorizontalLayerLine(0, "wall");
-            layers[2].SetHorizontalLayerLine(1, "topWallBrink");
-            layers[2].SetOnUniqueLayerID(1, 0, "0");
-            layers[2].SetOnUniqueLayerID(1, layers[2].LayerLengthX - 1, "0");
-            layers[2].SetOnUniqueLayerID(0, 0, "leftCorner");
-            layers[2].SetOnUniqueLayerID(0, layers[2].LayerLengthX - 1, "rightCorner");
-        }       
     }
-
-    // Layer ============================================================================================================================
 
     public class Layer
     {
@@ -88,7 +83,7 @@ namespace MapGeneration
 
         internal Layer(int heightY, int lengthX)
         {
-            this.LayerObjectMap = new string[heightY + 1, lengthX];
+            this.LayerObjectMap = new string[heightY, lengthX];
             this.LayerHeightY = heightY;
             this.LayerLengthX = lengthX;
         }
@@ -147,6 +142,14 @@ namespace MapGeneration
             }
         }
 
+        public void SetUniqueCorners(string leftTopCorner, string rightTopCorner, string leftBottomCorner, string rightBottomCorner) 
+        {
+            LayerObjectMap[0, 0] = leftBottomCorner;
+            LayerObjectMap[0, LayerLengthX - 1] = rightBottomCorner;
+            LayerObjectMap[LayerHeightY - 1, 0] = leftTopCorner;
+            LayerObjectMap[LayerHeightY - 1, LayerLengthX - 1] = rightTopCorner;
+        }
+
         public void SetBottomLayerCorners(string objectName)
         {
             LayerObjectMap[0, 0] = objectName;
@@ -162,7 +165,132 @@ namespace MapGeneration
         public void SetOnUniqueLayerID(int y, int x, string objectName) 
         {
             LayerObjectMap[y, x] = objectName;
-        }        
+        }
+
+        public void SetOnRandomLayerID(string objectName) //Randomly sets given object inside given area
+        {
+            for (int y = 0; y < LayerHeightY; y++) 
+            {
+                for (int x = 0; x < LayerLengthX; x++) 
+                {
+                    if ((Random.Range(0, 5) == 1) && x != 0 && x != LayerLengthX - 1)  //Randomizer of inner objects
+                    {
+                        LayerObjectMap[y, x] = objectName;
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Class that is responible for sorting all data about rooms on game level
+    class Location
+    {
+        public readonly string[,,] LocationObjectMap;
+        public readonly List<Layer> LocationLayers;
+        public readonly int NumberOfRooms;
+        public readonly int SpaceBetweenRooms;
+        public readonly int LocationHeightY;
+        public readonly int LocationLengthX;
+        public readonly int LocationLevelZ;
+
+        private Room[] rooms;                
+        private int minRoomHeightY;
+        private int minRoomLengthX;
+        private int maxRoomHeightY;
+        private int maxRoomLengthX;
+
+        public Location(int numberOfRooms, int spaceBetweenRooms, int minRoomHeightY, int maxRoomHeightY, int minRoomLengthX, int maxRoomLengthX)
+        {
+            this.minRoomHeightY = minRoomHeightY;
+            this.maxRoomHeightY = maxRoomHeightY;
+            this.minRoomLengthX = minRoomLengthX;
+            this.maxRoomLengthX = maxRoomLengthX;
+            NumberOfRooms = numberOfRooms;
+            SpaceBetweenRooms = spaceBetweenRooms;
+            rooms = new Room[numberOfRooms];
+            
+            generateRooms();
+            LocationLevelZ = maxNumberOfLayersLevelsZ();
+            LocationHeightY = getLocationHeightY();
+            LocationLengthX = getLocationLengthX();            
+            LocationObjectMap = new string[LocationLevelZ, LocationHeightY, LocationLengthX];
+
+            createLayerObjectMap();
+        }
+
+        public void createLayerObjectMap() //Shit is here
+        {
+            int spacingY = 0;
+            int spacingX = 0;
+
+            for (int roomNumber = 0; roomNumber < NumberOfRooms; roomNumber++)
+            {
+                if (roomNumber != 0)
+                {
+                    spacingX += rooms[roomNumber].RoomLengthX;
+                    spacingY += rooms[roomNumber].RoomHeightY;
+                }
+
+                for (int layerNumber = 0; layerNumber < rooms[roomNumber].Layers.Count; layerNumber++)
+                {
+                    for (int y = 0; y < rooms[roomNumber].Layers[layerNumber].LayerHeightY; y++) 
+                    {
+                        for (int x = 0; x < rooms[roomNumber].Layers[layerNumber].LayerLengthX; x++) 
+                        {
+                            LocationObjectMap[layerNumber, y + spacingY, x + spacingX] = rooms[roomNumber].Layers[layerNumber].LayerObjectMap[y, x];               
+                        }
+                    }     
+                }
+            }
+        }
+
+        private void generateRooms()
+        {
+            for (int i = 0; i < NumberOfRooms; i++) 
+            {
+                rooms[i] = new Room(Random.Range(minRoomHeightY, maxRoomHeightY), Random.Range(minRoomLengthX, maxRoomLengthX));
+            }
+        }
+
+        private int getLocationHeightY() 
+        {
+            int locationHeightY = 0;
+            for (int i = 0; i < NumberOfRooms; i++)
+            {
+                locationHeightY += rooms[i].RoomHeightY;
+                locationHeightY += SpaceBetweenRooms;
+            }
+
+            return locationHeightY;
+        }
+
+        private int getLocationLengthX()
+        {
+            int locationLengthX = 0;
+            for (int i = 0; i < NumberOfRooms; i++)
+            {
+                locationLengthX += rooms[i].RoomLengthX;
+                locationLengthX += SpaceBetweenRooms;
+            }
+
+            return locationLengthX;
+        }
+
+        private int maxNumberOfLayersLevelsZ() 
+        {
+            int layersCount = 0; //Variable that is responsible for counting layers in rooms
+
+            for (int roomNumber = 0; roomNumber < NumberOfRooms; roomNumber++) //Looks through all rooms in map 
+            {
+                for (int layerInRoom = 0; layerInRoom < rooms[roomNumber].Layers.Count; layerInRoom++) //Looks through each layer in room and counts number if room layers
+                {
+                    layersCount++;            
+                }                
+            }
+
+            return layersCount;
+        }
     }
 
     class LayerIsBiggerThanRoomException : System.Exception
@@ -181,8 +309,42 @@ namespace MapGeneration
             TO DO LIST
 1. add corners - done
 2. left, right, top and down walls - done;
-3. add layers
+3. add layers - done;
 4. add doors
-5. add inner objects
-6. add top side of the wall
+5. add inner objects - need rework;
+6. add top side of the wall - done;
+7. add map generation - done;
+8. add spacing;
+9. rewrite Room generation !!!!!!!!!
+*/
+
+
+/*
+    protected void instRoom()
+        {
+            //======================Layer 0======================
+            AddRoomLayer(RoomHeightY - 1, RoomLengthX - 1);
+            Layers[0].FillWholeLayerMap("floor");
+            Layers[0].SetHorizontalLayerLine(Layers[0].LayerHeightY - 1, "wall");
+            Layers[0].SetVerticalLayerLine(0, "leftWall");
+            Layers[0].SetVerticalLayerLine(Layers[0].LayerLengthX - 1, "rightWall");
+
+            //======================Layer 1======================
+            AddRoomLayer(RoomHeightY - 2, RoomLengthX - 2);
+            Layers[1].SetOnRandomLayerID("innerObject");
+
+            //======================Layer 2======================
+            AddRoomLayer(RoomHeightY, RoomLengthX - 1);
+            //Sets top side of the room            
+            Layers[2].SetHorizontalLayerLine(Layers[2].LayerHeightY - 1, "topWallBrink");
+            Layers[2].SetOnUniqueLayerID(Layers[2].LayerHeightY - 1, 0, "leftTopBrinkCorner");
+            Layers[2].SetOnUniqueLayerID(Layers[2].LayerHeightY - 1, Layers[2].LayerLengthX - 1, "rightTopBrinkCorner");
+            //Sets bottom sides
+            Layers[2].SetHorizontalLayerLine(0, "wall");
+            Layers[2].SetHorizontalLayerLine(1, "topWallBrink");
+            Layers[2].SetOnUniqueLayerID(1, 0, "0");                            // removes odd objects            
+            Layers[2].SetOnUniqueLayerID(1, Layers[2].LayerLengthX - 1, "0");   // removes odd objects
+            Layers[2].SetOnUniqueLayerID(0, 0, "leftCorner");
+            Layers[2].SetOnUniqueLayerID(0, Layers[2].LayerLengthX - 1, "rightCorner");
+        }       
 */
